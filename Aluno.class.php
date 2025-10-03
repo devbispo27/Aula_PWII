0b06d528e6737daf0de80bb6460773a6beaeed8a
devbispo27 <arthurbispodossantos7@gmail.com>
2025-10-03 14:35:49 -0300

    Update Aluno.class.php

--- Aluno.class.php
+++ Aluno.class.php
@@ -1,11 +1,11 @@
 <?php
 class Aluno{
-    private $id;
-    private $rm;
-    private $nome;
-    private $email;
-    private $cpf;
-    private $pdo;
+    private $arthurbid;
+    private $arthurbrm;
+    private $arthurbnome;
+    private $arthurbemail;
+    private $arthurbcpf;
+    private $arthurbpdo;
 
     public function conectar(){
         $dns    = "mysql:dbname=EtimPwiiAluno;host=localhost";
@@ -21,62 +21,63 @@ class Aluno{
     }
 
     public function getId(){
-        return $this->id;
+        return $this->arthurbid;
     }
     public function getRm(){
-        return $this->rm;
+        return $this->arthurbrm;
     }
     public function getNome(){
-        return $this->nome;
+        return $this->arthurbnome;
     }
     public function getEmail(){
-        return $this->email;
+        return $this->arthurbemail;
     }
     public function getCpf(){
-        return $this->cpf;
+        return $this->arthurbcpf;
     }
 
-    public function setRm($rm){
-        $this->rm = $rm ;
+    public function setRm($arthurbrm){
+        $this->rm = $arthurbrm ;
     }
-    public function setNome($nome){
-        $this->nome = $nome ;
+    public function setNome($arthurbnome){
+        $this->nome = $arthurbnome ;
     }
-    public function setEmail($email){
-        $this->email = $email ;
+    public function setEmail($arthurbemail){
+        $this->email = $arthurbemail ;
     }
-    public function setCpf($cpf){
-        $this->cpf = $cpf ;
+    public function setCpf($arthurbcpf){
+        $this->cpf = $arthurbcpf ;
     }
 
-    public function cadastrar($rm, $nome, $email, $cpf){
+    public function cadastrar($arthurbrm, $arthurbnome, $arthurbemail, $arthurbcpf){
         # criar uma variavel com a conulta SQL
-        $sql = "INSERT INTO aluno set rm = :r, nome = :n, email = :e, cpf = :c";
+        $arthursql = "INSERT INTO aluno set rm = :r, nome = :n, email = :e, cpf = :c";
         
         # se o metodo tem parametros, temos que usar o apelido para passar os valores 
         # e chamar o metodo prepare do PDO
-        $sql = $this->pdo->prepare($sql);
+        $arthurbsql = $this->pdo->prepare($arthurbsql);
 
         #para cada apelido, ligar com o valor do parametro passado
-        $sql-> bindValue(":r", $rm);
-        $sql-> bindValue(":n", $nome);
-        $sql-> bindValue(":e", $email);
-        $sql-> bindValue(":c", $cpf);
+        $sql-> bindValue(":r", $arthurbrm);
+        $sql-> bindValue(":n", $arthurbnome);
+        $sql-> bindValue(":e", $arthurbemail);
+        $sql-> bindValue(":c", $arthurbcpf);
 
         #executar o comando
         return $sql->execute();
     }
 
-    public function consultar($email){
-        $sql = "SELECT *FROM aluno WHERE email = :e";
-        $sql = $this->pdo->prepare($sql);
-        $sql-> bindValue(":e", $email);
-        $sql->execute();
+    public function consultar($arthuremail){
+        $arthurbsql = "SELECT *FROM aluno WHERE email = :e";
+        $arhturbsql = $this->pdo->prepare($arthurbsql);
+        $arthurbsql-> bindValue(":e", $arthurbemail);
+        $arthurbsql->execute();
 
-        return $sql->rowCount() > 0;
+        return $arthurbsql->rowCount() > 0;
         
     }
 
 
 }
+
 
