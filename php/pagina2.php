eb72f97de24601a9ad7dc8395da9ea46b3eaa73c
devbispo27 <arthurbispodossantos7@gmail.com>
2025-10-03 14:39:27 -0300

    Update pagina2.php

--- php/pagina2.php
+++ php/pagina2.php
@@ -2,8 +2,9 @@
 session_start();
 
 if( isset($_SESSION['nome'])){
-    $nome = $_SESSION['nome'];
-    echo "<h1>Oi $nome, bem vindo!";
+    $arthurbnome = $_SESSION['nome'];
+    echo "<h1>Oi $arthurbnome, bem vindo!";
 }else{
     echo"<h1>Que pena, voce nao esta logado!!";
+
 }
